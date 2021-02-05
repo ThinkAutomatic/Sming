8122aefd7c43decbd1a25bb725c7786e318fc2cf
Steve Harris <steve@thinkautomatic.com>
2021-02-05 01:04:44 -0800

    Remove LuminodeLib as a submodule

--- Sming/Libraries/LuminodeLib/README.rst
+++
@@ -1,4 +0,0 @@
-LuminodeLib
-============
-
-Library for ESP8266 based Think Automatic devices to connect to the platform.
--- Sming/Libraries/LuminodeLib/component.mk
+++
@@ -1,3 +0,0 @@
-COMPONENT_SUBMODULES := LuminodeLib
-COMPONENT_SRCDIRS += LuminodeLib/src
-COMPONENT_INCDIRS += LuminodeLib/src/include
